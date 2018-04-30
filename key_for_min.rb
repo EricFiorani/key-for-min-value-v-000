2d816776d2e61356136b10b064789ff17ba79da2
Eric Fiorani <efiorani305@gmail.com>
2018-04-30 15:42:11 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,5 @@
 # Given a hash with numeric values, return the key for the smallest value
 key_for_min_value = {:blake => 500, :ashley => 2, :adam => 1}
 def key_for_min_value(name_hash)
-
+  key_for_min_value(name_hash)
 end
